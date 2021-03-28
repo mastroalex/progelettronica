b3e7e7e1e4494174e6404cdbab1896f815b0da7f
mastroalex <76497903+mastroalex@users.noreply.github.com>
2021-03-28 15:57:44 +0200

    Create get_angle_esempio_libreria.ino

---
+++ GY-521 file/get_angle_esempio_libreria/get_angle_esempio_libreria.ino
@@ -0,0 +1,27 @@
+#include <MPU6050_tockn.h>
+#include <Wire.h>
+
+MPU6050 mpu6050(Wire);
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
+}
+
+void loop() {
+  mpu6050.update();
+  Serial.print("angleX : ");
+  Serial.print(mpu6050.getAngleX());
+  Serial.print("\tangleY : ");
+  Serial.print(mpu6050.getAngleY());
+  Serial.print("\tangleZ : ");
+  Serial.print(mpu6050.getAngleZ());
+  Serial.print("\ttemp : "); 
+  Serial.print(mpu6050.getTemp()); 
+  Serial.print("\ttemp2 : "); 
+  Serial.println(mpu6050.getRawTemp());
+
+
+}
