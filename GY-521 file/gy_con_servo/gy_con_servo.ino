e422899549556c9061cc8ff924054d3ce045a0e1
mastroalex <76497903+mastroalex@users.noreply.github.com>
2021-03-28 17:27:13 +0200

    Update gy_con_servo.ino

--- GY-521 file/gy_con_servo/gy_con_servo.ino
+++ GY-521 file/gy_con_servo/gy_con_servo.ino
@@ -4,7 +4,7 @@
 float tempmpu = 0; // temperatura MPU5060
 float anglex = 0;  //angolo lungo x MPU5060
 int t1 = 0; // timer per la stampa dei valori
-int pausa = 200 ; // pausa per il serial print
+int pausa = 400 ; // pausa per il serial print
 
 MPU6050 mpu6050(Wire);
 
@@ -13,18 +13,18 @@ void setup() {
   Wire.begin();
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
- t1 = millis();
+  t1 = millis();
 }
 
 void loop() {
-    mpu6050.update();
+  mpu6050.update();
   tempmpu = mpu6050.getTemp(); // prende la temperatura
   anglex = mpu6050.getAngleX(); // prende l'angolo lungo x
- if (millis() - t1 > pausa){
-  Serial.print("angleX : ");
-  Serial.print(anglex);
-  Serial.print("\t temp : ");
-  Serial.println(tempmpu);
+  if (millis() - t1 > pausa) {
+    Serial.print("angleX : ");
+    Serial.print(anglex);
+    Serial.print("\t temp : ");
+    Serial.println(tempmpu);
   }
-  t1=millis();
+  t1 = millis();
 }
